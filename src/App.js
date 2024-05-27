6dcc3f414237d1ceba4d939b62a5bb8d47c5bc72
Abdullah Khan <abdullah5601013@gmail.com>
2024-05-27 12:11:18 +0500

    Authtoken integrated

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPage from './screens/LoginPage'; 
 import SignupPage from './screens/SignupPage';
+import PrivateRoute from "./screens/PrivateRoute.js";
 import Welcome from './screens/Welcome'
 import './App.css';
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const authToken = localStorage.getItem("authToken");
+    if (authToken) {
+      setIsAuthenticated(true);
+    }
+  }, []);
+
   return (
     <BrowserRouter>
     <div className="App">
       <Routes>
 
         <Route path="/" element={<LoginPage />} />
-        <Route path="/welcomePage" element={<Welcome />} />
+        <Route
+            path="/welcomePage"
+            element={
+              <PrivateRoute
+                element={<Welcome/>}
+                isAuthenticated={isAuthenticated}
+              />
+            }
+          />
+       
         </Routes>
 
         <Routes>
